4811096c5fc82fcf5bc0cff57d554e2d338caf2e
bang <sugiaichigo00781@gmail.com>
2026-04-25 16:43:56 +0700

    feat(plugin): add fold

--- init.lua
+++ init.lua
@@ -431,6 +431,7 @@ require('lazy').setup({
   require 'custom.plugins.copilot',
   require 'custom.plugins.avante',
   require 'custom.plugins.session',
+  require 'custom.plugins.ufo',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
